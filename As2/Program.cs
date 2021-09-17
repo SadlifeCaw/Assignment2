d82ec0c2a068661a41f35ab438ca3b7ad9a59908
Casper Wassar Skourup <caws@itu.dk>
2021-09-17 13:58:58 +0200

    Setup folder structure

---
+++ As2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace As2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
