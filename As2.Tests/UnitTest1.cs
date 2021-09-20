a55856e9b497a5eda64d7341b5483818474ad89b
Oreo <90245364+EatOreo@users.noreply.github.com>
2021-09-20 10:18:19 +0200

    implement and test Student (#3)
    Co-authored-by: luhj <luhj@itu.dk>

--- As2.Tests/UnitTest1.cs
+++ As2.Tests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using System;
 using Xunit;
 
@@ -6,9 +7,17 @@ namespace As2.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Student_ToString()
         {
+            Student stud = new Student(13, "Lars", "Hansen");
 
+            string expected = "Id: 13 Name: Lars Hansen Status: New";
+            string actual = stud.ToString();
+
+            Assert.Equal(expected, actual);
+            
+            // stud.GraduationDate = date1;
+            // System.Console.WriteLine(stud.ToString());
         }
     }
 }
